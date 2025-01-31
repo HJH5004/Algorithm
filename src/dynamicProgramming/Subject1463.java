cbfabda10be620e9acb4aedec9c6b18dc5462c1e
Curry5004 <yunjin5004@naver.com>
2025-02-01 06:27:45 +0900

    * 25 02 01
    문제 풀이

---
+++ src/dynamicProgramming/Subject1463.java
@@ -0,0 +1,74 @@
+package dynamicProgramming;
+
+import java.util.HashMap;
+
+/**
+ * 문제
+ * 정수 X에 사용할 수 있는 연산은 다음과 같이 세 가지 이다.
+ *
+ * X가 3으로 나누어 떨어지면, 3으로 나눈다.
+ * X가 2로 나누어 떨어지면, 2로 나눈다.
+ * 1을 뺀다.
+ * 정수 N이 주어졌을 때, 위와 같은 연산 세 개를 적절히 사용해서 1을 만들려고 한다. 연산을 사용하는 횟수의 최솟값을 출력하시오.
+ *
+ * 입력
+ * 첫째 줄에 1보다 크거나 같고, 106보다 작거나 같은 정수 N이 주어진다.
+ *
+ * 출력
+ * 첫째 줄에 연산을 하는 횟수의 최솟값을 출력한다.
+ *
+ * 예제 입력 1
+ * 2
+ * 예제 출력 1
+ * 1
+ * 예제 입력 2
+ * 10
+ * 예제 출력 2
+ * 3
+ * 힌트
+ *
+ * 출처 : https://www.acmicpc.net/problem/16194
+ *
+ * */
+
+public class Subject1463 {
+    public static void main(String[] args) {
+        /**
+         * 스캐너 구현
+         * */
+        int value = 10;
+
+
+        /**
+         * 탈출문 조건
+         *      연산 값이 1이 될 때 연산한 횟수를 return한다.
+         * */
+        int count = 0;
+        int result = solution(value, count);
+
+        System.out.println(count);
+
+
+    }
+
+    private static <Map> int solution(int value, int count) {
+
+        //탈출 조건 = value의 값이 1 일때 return 한다.
+        if (value == 1) {
+            return count;
+        }
+
+        /**
+         * 조건 1. 연산을 마쳤을 때 정수 값이 나와야 한다.
+         * 조건 2. 각 연산을 하였을 때 그 값이 가장 작은 값이여야함.
+         *      -> 왜? 로직의 최소값을 찾는 것이 최종 목표 이기 떄문
+         * */
+        HashMap<String, Integer> resultMap = new HashMap<>();
+
+        if(value % 3 == 0){
+            resultMap.put("3", value/3);
+        }else if(value % 2 == 0){}
+
+        return 0;
+    }
+}
